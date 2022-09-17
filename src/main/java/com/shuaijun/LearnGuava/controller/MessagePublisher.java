1f00031e36050e43217665f2c145ed14a1b036f9
帥　軍 <shuaijunqian0203@gmail.com>
2022-09-17 22:30:08 +0900

    rabbitmqの対応追加確認済み

---
+++ src/main/java/com/shuaijun/LearnGuava/controller/MessagePublisher.java
@@ -0,0 +1,30 @@
+package com.shuaijun.LearnGuava.controller;
+
+
+import com.shuaijun.LearnGuava.config.MQConfig;
+import com.shuaijun.LearnGuava.entity.CustomerMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@ComponentScan
+public class MessagePublisher {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/publish")
+    public String publish(@RequestBody CustomerMessage customerMessage){
+        customerMessage.setMessageID(UUID.randomUUID().toString());
+        customerMessage.setMessageDate(new Date());
+        rabbitTemplate.convertAndSend(MQConfig.EXCHANGE,MQConfig.ROUTING_KEY,customerMessage);
+        return "message published.";
+    }
+}
---
+++ src/main/java/com/shuaijun/LearnGuava/entity/CustomerMessage.java
@@ -0,0 +1,17 @@
+package com.shuaijun.LearnGuava.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CustomerMessage {
+    private String messageID,message;
+    private Date messageDate;
+}
